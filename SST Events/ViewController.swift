dae7e17a2d53cdf9f8397b2bed6ec4b82b44a96a
Troy Tan <troytanhongkai@troys-macbook-pro.local>
2018-08-31 22:43:57 +0800

    Ui more or less done

--- SST Events/ViewController.swift
+++ SST Events/ViewController.swift
@@ -15,11 +15,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         signUpBtn.layer.borderWidth = 2
-        signUpBtn.layer.borderColor = UIColor.blue.cgColor
-        signUpBtn.layer.cornerRadius = 20
+        signUpBtn.layer.borderColor = #colorLiteral(red: 0.3098039216, green: 0.4509803922, blue: 0.6352941176, alpha: 1)
+        signUpBtn.layer.cornerRadius = 15
         logInBtn.layer.borderWidth = 2
-        logInBtn.layer.borderColor = UIColor.blue.cgColor
-        logInBtn.layer.cornerRadius = 20
+        logInBtn.layer.borderColor = #colorLiteral(red: 0.3098039216, green: 0.4509803922, blue: 0.6352941176, alpha: 1)
+        logInBtn.layer.cornerRadius = 15
         // Do any additional setup after loading the view, typically from a nib.
     }
 
